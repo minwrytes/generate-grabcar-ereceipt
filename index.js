fd0da577e9c1d1abbdf3ee9ce3e38f4b5f8b8dac
Deniel Ariesta <deniel.ariesta@gmail.com>
2017-03-14 18:18:08 +0800

    added source

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-var htmlFile = require('fs');
+var fs = require('fs');
 var path = require('path');
+
+var htmlFilePath = path.join(__dirname, 'grab-receipt.html');
+var sourceFilePath = path.join(__dirname, 'source.txt');
+
+fs.readFile(sourceFilePath, { encoding: 'utf-8' } , function(sourceErr, source) {
+  if (!sourceErr) {
+    console.log(source);
+    fs.readFile(htmlFilePath, { encoding: 'utf-8' } , function(htmlErr, htmlData) {
+      if (!htmlErr) {
+        htmlData = htmlData.toString();
+        htmlData = htmlData.replace(/RM 15/g, 'RM 45');
+      }
+    });
+  }
+});
