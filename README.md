5b2633f650b7182fa50b1af5871a29a7e33e9d07
Deniel Ariesta <deniel.ariesta@gmail.com>
2020-10-11 19:34:11 +0700

    update script to use parser, and update csv format

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # generate-grabcar-ereceipt
-generate grabcar e-receipt based on csv input file. 
-Use with your own risk
+This script is used to generate GrabCar E-Receipt. Use at your own risk.
 
 ### Installation
 Make sure you have Node JS installed
@@ -9,3 +8,6 @@ Make sure you have Node JS installed
 1. Fill in the data in csv file. follow the csv template
 2. run `node index.js` in terminal. It will generate the html files. 
 3. open the html files one by one if you want to print or convert it to pdf
+
+### TODOs
+- [ ] An option to generate pdf automatically
--- index.js
+++ index.js
@@ -1,54 +1,71 @@
-var fs = require('fs');
-var path = require('path');
-var readLine = require('readline');
+const fs = require('fs');
+const path = require('path');
+const csvParse = require('csv-parse');
 
-var htmlFilePath = path.join(__dirname, 'grab-receipt.html');
-var lineReader = readLine.createInterface({
-  input: fs.createReadStream('source.csv', {start: 102})
-});
+const htmlFilePath = path.join(__dirname, 'grab-receipt.html');
 
-lineReader.on('line', function (orderData) {
-  fs.readFile(htmlFilePath, { encoding: 'utf-8' } , function(htmlErr, htmlData) {
-    if (!htmlErr) {
-      var splittedData = orderData.split(';');
-      var emailDate = splittedData[0],
-        amount = splittedData[1],
-        orderDate = splittedData[2],
-        type = splittedData[3],
-        driversName = splittedData[4],
-        bookingCode = splittedData[5],
-        pickupLocation = splittedData[6],
-        dropOffLocation = splittedData[7],
-        paymentMethod = splittedData[8];
-
-      htmlData = htmlData.toString();
-      // replace amount
-      htmlData = htmlData.replace(/RM 15/g, amount);
-      // replace payment method
-      htmlData = htmlData.replace('Cash', paymentMethod);
-      // replace email date
-      htmlData = htmlData.replace('emailDate', emailDate);
-      // replace orderDate
-      htmlData = htmlData.replace('orderDate', orderDate);
-      // replace type
-      htmlData = htmlData.replace('GrabCar', type);
-      // replace driver's name
-      htmlData = htmlData.replace('ABCName', driversName);
-      // replace booking code
-      htmlData = htmlData.replace('BookingCodeNumber', bookingCode);
-      // replace pickupLocation
-      htmlData = htmlData.replace('PickupLocation', pickupLocation);
-      // replace dropOffLocation
-      htmlData = htmlData.replace('dropOffLocation', dropOffLocation);
-
-      if (!!emailDate) {
-        fs.writeFile("dir/" + emailDate + ".html", htmlData, function(err) {
-          if (err)
-            return console.log(err);
-
-          console.log("The file was saved!");
+const lineReader = (sourceFileReader) => {
+  console.log('Starting...');
+  return new Promise((resolve, reject) => {
+    sourceFileReader
+      .pipe(csvParse({
+        delimiter: [','],
+        columns: true,
+        skip_lines_with_error: true
+      }))
+      .on('data', (item) => {
+        fs.readFile(htmlFilePath, { encoding: 'utf-8' } , (htmlErr, htmlData) => {
+          if (!htmlErr) {
+            const emailDate = item['email date'],
+              amount = item['amount'],
+              orderDate = item['order date'],
+              type = item['type'],
+              driversName = item['driver name'],
+              bookingCode = item['booking code'],
+              pickupLocation = item['pickup location'],
+              dropOffLocation = item['drop off'],
+              paymentMethod = item['payment method'],
+              yourEmailAddress = item['your email address'],
+              issuedTo = item['issued to'];
+      
+            htmlData = htmlData.toString();
+            htmlData = htmlData.replace(/RM 15/g, amount);
+            htmlData = htmlData.replace('Cash', paymentMethod);
+            htmlData = htmlData.replace('emailDate', emailDate);
+            htmlData = htmlData.replace('orderDate', orderDate);
+            htmlData = htmlData.replace('GrabCar', type);
+            htmlData = htmlData.replace('ABCName', driversName);
+            htmlData = htmlData.replace('BookingCodeNumber', bookingCode);
+            htmlData = htmlData.replace('PickupLocation', pickupLocation);
+            htmlData = htmlData.replace('dropOffLocation', dropOffLocation);
+            htmlData = htmlData.replace(/your_email_address/g, yourEmailAddress);
+            htmlData = htmlData.replace(/issued_to/g, issuedTo);
+      
+            if (emailDate) {
+              fs.writeFile("dir/" + emailDate + ".html", htmlData, (err) => {
+                if (err) {
+                  return console.log(err);
+                }
+      
+                console.log("The file was saved!");
+              });
+            }
+          }
         });
-      }
-    }
+      })
+      .on('error', err => {
+        console.log(err);
+        reject();
+      })
+      .on('end', () => {
+        resolve();
+      })
   });
-});
+}
+
+const run = () => {
+  const sourceFileReader = fs.createReadStream('./source.csv');
+  return lineReader(sourceFileReader);
+}
+
+run();
